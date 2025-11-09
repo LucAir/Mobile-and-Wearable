5b12086f9f1a59dcb118afa9b578204d81952d51
24luca24 <luca.airaghi24@gmail.com>
2025-11-09 14:10:01 +0100

    Improved safety in UserData class, ensuring NonNull parameters

--- app/src/main/java/com/example/navigationbarstarter/database/UserData.java
+++ app/src/main/java/com/example/navigationbarstarter/database/UserData.java
@@ -1,22 +1,41 @@
 package com.example.navigationbarstarter.database;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "userData")
+@Entity(tableName = "userData",
+        //Creating index for fast access (retrieve user by email/username)
+        //Useful for concurrency also, because are unique, so even if a thread write Luca as username,
+        //and there is already, it will be rejected.
+        indices = {
+            @Index(value = "username", unique = true),
+            @Index(value = "email", unique = true)
+        })
 public class UserData {
 
     @PrimaryKey(autoGenerate = true)
     private int id;
 
+    @NonNull
+    @ColumnInfo(name = "username")
     private String name;
+
+    @ColumnInfo(name = "surname")
     private String surname;
+
+    @NonNull
+    @ColumnInfo(name = "username")
     private String username;
+
+    @NonNull
+    @ColumnInfo(name = "password")
     private String password;
 
 
-    public UserData(String name, String surname, String username, String password){
+    public UserData(@NonNull String name, String surname,  @NonNull String username, @NonNull String password){
         this.name = name;
         this.surname = surname;
         this.username = username;
@@ -31,11 +50,12 @@ public class UserData {
         this.id = id;
     }
 
+    @NonNull
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(@NonNull String name) {
         this.name = name;
     }
 
@@ -47,19 +67,21 @@ public class UserData {
         this.surname = surname;
     }
 
+    @NonNull
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(@NonNull String username) {
         this.username = username;
     }
 
+    @NonNull
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(@NonNull String password) {
         this.password = password;
     }
 }
