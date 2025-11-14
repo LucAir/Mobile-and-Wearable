b7a89e00cf7bccf455c195bfc17a560c2b4f6297
24luca24 <luca.airaghi24@gmail.com>
2025-11-14 10:23:08 +0100

    guardin branch

--- app/src/main/java/com/example/navigationbarstarter/database/AppDatabase.java
+++ app/src/main/java/com/example/navigationbarstarter/database/AppDatabase.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
 
 @Database(entities = {UserData.class, ModeChange.class}, version = 4, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
@@ -15,21 +17,24 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDataDao userDataDao();
     public abstract ModeChangeDao modeChangeDao();
 
-    public static synchronized AppDatabase getInstance(Context context) {
-        if (instance == null) {
-            synchronized (LOCK) {
-                if (instance == null) {
-                    instance = Room.databaseBuilder(
-                                    context.getApplicationContext(),
-                                    AppDatabase.class,
-                                    "app_database"
-                            )
-                            .fallbackToDestructiveMigration()
-                            .build();
+    @TypeConverters({Converters.class})
+    public abstract class AppDatabase extends RoomDatabase {
+        public static synchronized AppDatabase getInstance(Context context)
+            if (instance == null) {
+                synchronized (LOCK) {
+                    if (instance == null) {
+                        instance = Room.databaseBuilder(
+                                        context.getApplicationContext(),
+                                        AppDatabase.class,
+                                        "app_database"
+                                )
+                                .fallbackToDestructiveMigration()
+                                .build();
+                    }
                 }
             }
+            return instance;
         }
-        return instance;
     }
 
     public static void destroyInstance() {
