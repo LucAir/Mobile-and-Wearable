f0b25cd899305e0dab300c5dbabe2ff37d351a68
FraDeVito <francesco.de.vito@usi.ch>
2025-12-05 07:24:43 +0100

    fix: removed header

--- app/src/main/java/com/example/navigationbarstarter/MainActivity.java
+++ app/src/main/java/com/example/navigationbarstarter/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_guardian, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
